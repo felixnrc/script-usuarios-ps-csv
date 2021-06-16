61605a48db845a0a02f5054393450490f57e72ad
Felix Natanael Rojas <42502767+felixnrc@users.noreply.github.com>
2021-06-16 18:25:03 -0300

    agregando Usuarios a Escritorio remoto
    Add-LocalGroupMember -Group "Usuarios de escritorio remoto" -Member $i.nombre

--- mk-usr.ps1
+++ mk-usr.ps1
@@ -1,10 +1,9 @@
 #Creación de usuarios de forma masiva.
-	$usuarios=Import-Csv -Path c:\PSD-User\usuario.csv
-	foreach  ($i in $usuarios)
+$usuarios=Import-Csv -Path c:\PSD-User\usuario.csv
+foreach  ($i in $usuarios)
 	{
-		$clave=ConvertTo-SecureString $i.contra -AsPlainText -Force
-    #Se crea el Usuario con lo cargado en usuario.csv
-    New-LocalUser $i.nombre -FullName $i.nombrecompleto -Description $i.descripcion -Password $clave -AccountNeverExpires -PasswordNeverExpires
-		#Se agrega los usuario creados al siguiente grupo (grupo default usuario)
-    Add-LocalGroupMember -Group usuarios -Member $i.nombre
+	$clave=ConvertTo-SecureString $i.contra -AsPlainText -Force
+    	New-LocalUser $i.nombre -FullName $i.nombrecompleto -Description $i.descripcion -Password $clave -AccountNeverExpires -PasswordNeverExpires
+    	Add-LocalGroupMember -Group usuarios -Member $i.nombre
+   	Add-LocalGroupMember -Group "Usuarios de escritorio remoto" -Member $i.nombre
 	}
